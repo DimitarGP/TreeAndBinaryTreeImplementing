eecb51d0c03e67244c3956c450ab6a6635d9bb17
DimitarGP <petkov.dg@gmail.com>
2018-09-27 13:30:44 +0300

    Home work of SoftUni. Play with tree data structure.

---
+++ Trees/BinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.LefChild = leftChild;
+        this.RightChild = rightChild;
+    }
+
+    public T Value { get; set; }
+    public BinaryTree<T> LefChild { get; set; }
+    public BinaryTree<T> RightChild { get; set; }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + this.Value);
+
+        if (this.LefChild != null)
+        {
+            this.LefChild.PrintIndentedPreOrder(indent + 2);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(indent + 2);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this,action);
+    }
+
+    private void EachInOrder(BinaryTree<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        else
+        {
+            this.EachInOrder(node.LefChild,action);
+            action(node.Value);
+            this.EachInOrder(node.RightChild,action);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        EachPostOrder(this, action);
+    }
+
+    private void EachPostOrder(BinaryTree<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachPostOrder(node.LefChild, action);
+        this.EachPostOrder(node.RightChild, action);
+        action(node.Value);
+    }
+}
---
+++ Trees/PlayWithTrees.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class PlayWithTrees
+{
+    static void Main()
+    {
+        var tree =
+            new Tree<int>(7,
+                new Tree<int>(19,
+                    new Tree<int>(1),
+                    new Tree<int>(12),
+                    new Tree<int>(31)),
+                new Tree<int>(21),
+                new Tree<int>(14,
+                    new Tree<int>(23),
+                    new Tree<int>(6)));
+
+        Console.WriteLine("Tree (indented):");
+        tree.Print();
+
+        Console.Write("Tree nodes:");
+        tree.Each(c => Console.Write(" " + c));
+        Console.WriteLine();
+
+        Console.WriteLine();
+
+        var bt =
+            new BinaryTree<int>(7,
+                new BinaryTree<int>(5,
+                    new BinaryTree<int>(1),
+                    new BinaryTree<int>(6)),
+                new BinaryTree<int>(9,
+                    new BinaryTree<int>(8),
+                    new BinaryTree<int>(15)));
+
+        bt.PrintIndentedPreOrder();
+
+        Console.Write("Binary tree (indented, pre-order):");
+        bt.EachInOrder(c => Console.Write(" " + c));
+        Console.WriteLine();
+
+        Console.Write("Binary tree nodes (post-order):");
+        bt.EachPostOrder(c => Console.Write(" " + c));
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        var binaryTree =
+            new BinaryTree<string>("*",
+                new BinaryTree<string>("+",
+                    new BinaryTree<string>("3"),
+                    new BinaryTree<string>("2")),
+                new BinaryTree<string>("-",
+                    new BinaryTree<string>("9"),
+                    new BinaryTree<string>("6")));
+
+        Console.WriteLine("Binary tree (indented, pre-order):");
+        binaryTree.PrintIndentedPreOrder();
+
+        Console.Write("Binary tree nodes (in-order):");
+        binaryTree.EachInOrder(c => Console.Write(" " + c));
+        Console.WriteLine();
+
+        Console.Write("Binary tree nodes (post-order):");
+        binaryTree.EachPostOrder(c => Console.Write(" " + c));
+        Console.WriteLine();
+    }
+}
